b3f29fe8ffa99d9a0c326db76029f94708054f06
neptune <145970107+Bbelkasem@users.noreply.github.com>
2026-01-27 21:11:11 +0100

    Add basic weapon script template
    Added a basic weapon script template that I'll make inherit to the future weapons also added sprite sheet for a basic monster for enemy spawn and AI

---
+++ Assets/Game/GameplayMech/Weapon/WeaponController.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+//This is base script for all weapons (future weapons you gonna make idiot)
+public class WeaponController : MonoBehaviour
+{
+    [Header("Weapon Stats")]
+    public GameObject prefab;
+    public float damage;
+    public float speed;
+    public float CooldownDuration;
+    float currentCooldown;
+    public int pierce;  //This is the max amount of times a weapon can touch an enemy before disappearing (like can only touch 1 or 2 enemy's before disappearing )
+    void Start()
+    {
+        currentCooldown = CooldownDuration; // This shows that when player gets a weapon/new weapon it doesn't immediately starts shooting but the cooldown starts instead
+    }
+
+    
+    void Update()
+    {
+        currentCooldown -= Time.deltaTime;
+        if(currentCooldown <= 0f) //when cooldown hits 0, attack 
+        {
+            Attack();
+        }
+    }
+//Basically when Attack gets called it loops the cooldown again why the (currentCooldwon = CooldownDuration)
+    void Attack()
+    {
+        currentCooldown = CooldownDuration; 
+    }
+}
