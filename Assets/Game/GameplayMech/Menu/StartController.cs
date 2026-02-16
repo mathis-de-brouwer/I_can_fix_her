52a1d68b4d5d84eb30018d9a4f019477a977f9fb
mathis-de-brouwer <145472006+mathis-de-brouwer@users.noreply.github.com>
2026-02-16 15:27:47 +0100

    Revert "Changed the scene logic/order"
    This reverts commit 3ed472f5d437d3a31db219754416c84a14b69d17.

--- Assets/Game/GameplayMech/Menu/StartController.cs
+++ Assets/Game/GameplayMech/Menu/StartController.cs
@@ -3,27 +3,18 @@ using UnityEngine.SceneManagement;
 
 public class StartController : MonoBehaviour
 {
-    [Header("Scenes")]
-    [SerializeField] private string startSceneName = "SampleScene";
-
     public void OnStartClick()
     {
-        if (string.IsNullOrWhiteSpace(startSceneName))
-        {
-            Debug.LogError($"{nameof(StartController)}: Start scene name is empty.");
-            return;
-        }
-
-        SceneManager.LoadScene(startSceneName);
+         SceneManager.LoadScene("SampleScene");
     }
 
     public void OnExitClick()
     {
         // has to be changed later when we make an executable of the game for the time remaining we just gonna close unity
 
-#if UNITY_EDITOR
-        UnityEditor.EditorApplication.isPlaying = false;
-#endif
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #endif
 
         //Application.Quit(); // For later when we have an executable of the game 
     }
